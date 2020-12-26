ee43d75fec456a6e8a45dbe611c099377a3b009a
PurpleMyst <PurpleMyst@users.noreply.github.com>
2020-12-26 21:47:43 +0100

    perf(day10): remove LUT and use a Vec<u8> directly

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -1,33 +1,42 @@
-const LUT: [&str; 4 * 4] = [
-    "00", "01", "02", "03", "10", "11", "12", "13", "20", "21", "22", "23", "30", "31", "32", "33",
-];
+const PART1_TARGET: usize = 40;
+const PART2_TARGET: usize = 50;
 
 #[inline]
 pub fn solve() -> (usize, usize) {
-    let mut state = include_str!("input.txt").trim().to_string();
-    let mut next_state = String::with_capacity(state.len() * 2);
+    let mut state = include_str!("input.txt")
+        .trim()
+        .bytes()
+        .map(|b| b - b'0')
+        .collect::<Vec<_>>();
+
+    let mut next_state = Vec::with_capacity(state.len() * 2);
 
     let mut part1 = 0;
 
-    for i in 0..50 {
+    for i in 0..PART2_TARGET {
         next_state.clear();
+        state.reserve(state.len());
+        next_state.reserve(state.len() * 2);
 
-        let mut it = state.chars();
+        let mut it = state.iter().copied();
         let first = it.next().unwrap();
         let (cnt, group) = it.fold((1, first), |(cnt, group), digit| {
             if digit == group {
                 (cnt + 1, group)
             } else {
-                next_state.push_str(LUT[4 * cnt + (group as u8 - b'0') as usize]);
+                next_state.push(cnt);
+                next_state.push(group);
                 (1, digit)
             }
         });
 
-        next_state.push_str(LUT[4 * cnt + (group as u8 - b'0') as usize]);
+        next_state.push(cnt);
+        next_state.push(group);
 
-        state.clone_from(&next_state);
+        state.clear();
+        state.extend_from_slice(&next_state);
 
-        if i == 39 {
+        if i == PART1_TARGET - 1 {
             part1 = state.len();
         }
     }
