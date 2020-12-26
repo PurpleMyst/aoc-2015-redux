f2617f6f0705601dc22dd022527c6b8ddfaae013
PurpleMyst <PurpleMyst@users.noreply.github.com>
2020-12-26 21:15:54 +0100

    perf(day10): use lookup table to convert to string

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -1,3 +1,7 @@
+const LUT: [&str; 4 * 4] = [
+    "00", "01", "02", "03", "10", "11", "12", "13", "20", "21", "22", "23", "30", "31", "32", "33",
+];
+
 #[inline]
 pub fn solve() -> (usize, usize) {
     let mut state = include_str!("input.txt").trim().to_string();
@@ -10,15 +14,16 @@ pub fn solve() -> (usize, usize) {
 
         let mut it = state.chars();
         let first = it.next().unwrap();
-        let (cnt, last) = it.fold((1, first), |(cnt, group), digit| {
+        let (cnt, group) = it.fold((1, first), |(cnt, group), digit| {
             if digit == group {
                 (cnt + 1, group)
             } else {
-                next_state.push_str(&format!("{}{}", cnt, group));
+                next_state.push_str(LUT[4 * cnt + (group as u8 - b'0') as usize]);
                 (1, digit)
             }
         });
-        next_state.push_str(&format!("{}{}", cnt, last));
+
+        next_state.push_str(LUT[4 * cnt + (group as u8 - b'0') as usize]);
 
         state.clone_from(&next_state);
 
