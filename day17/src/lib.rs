58aa43e55aa4ad62f57c7ceb92c90d75953dfece
PurpleMyst <PurpleMyst@users.noreply.github.com>
2020-12-27 02:25:57 +0100

    perf(day17): implement DP solution

--- day17/src/lib.rs
+++ day17/src/lib.rs
@@ -1,47 +1,5 @@
-use std::cmp::Ordering;
-
 const CONTAINERS: usize = 20;
-
 const TARGET: u8 = 150;
-
-fn recurse(remaining: u8, containers: &[u8], len: usize, part2: &mut (usize, usize)) -> usize {
-    match containers.split_first().unwrap() {
-        (&container, &[]) => {
-            if remaining == container {
-                if part2.0 > len {
-                    *part2 = (len, 1);
-                } else if len == part2.0 {
-                    part2.1 += 1;
-                }
-
-                1
-            } else {
-                0
-            }
-        }
-
-        (container, rest) => {
-            // consider the case that we don't use this container
-            recurse(remaining, rest, len, part2)
-                + match remaining.cmp(container) {
-                    // if we can't use this container at this point, stop
-                    Ordering::Less => 0,
-                    // if we can fill this container exactly, do so
-                    Ordering::Equal => {
-                        if part2.0 > len {
-                            *part2 = (len, 1);
-                        } else if len == part2.0 {
-                            part2.1 += 1;
-                        }
-
-                        1
-                    }
-                    // otherwise, put all that we can in it and recurse
-                    Ordering::Greater => recurse(remaining - container, rest, len + 1, part2),
-                }
-        }
-    }
-}
 
 #[inline]
 pub fn solve() -> (usize, usize) {
@@ -53,7 +11,24 @@ pub fn solve() -> (usize, usize) {
         .zip(containers.iter_mut())
         .for_each(|(val, dest)| *dest = val);
 
-    let mut part2 = (usize::MAX, 0);
+    let mut counts = [[0; CONTAINERS + 1]; TARGET as usize + 1];
+    counts[0][0] = 1;
 
-    (recurse(TARGET, &containers, 0, &mut part2), part2.1)
+    for &container in containers.iter() {
+        for remaining in (0..=TARGET - container).rev() {
+            for i in (1..=CONTAINERS).rev() {
+                counts[(remaining + container) as usize][i] += counts[remaining as usize][i - 1];
+            }
+        }
+    }
+
+    let part1 = counts[TARGET as usize].iter().sum();
+
+    let part2 = counts[TARGET as usize]
+        .iter()
+        .copied()
+        .find(|&n| n != 0)
+        .unwrap();
+
+    (part1, part2)
 }
